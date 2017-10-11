6d2743723ea0efbd49eda25a1546141195089889
will-nemo <will.lambert96@gmail.com>
2017-10-11 18:06:50 -0400

    Updated WeaponFactory file. The strings for the new weapons wer

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,6 +9,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "SimpleAxe.h"
+#include "CrazyRandomSword.h"
+#include "ShillelaghOfFreedom.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -27,6 +30,14 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     if (name.compare("spear") == 0) {
         return new CommonSpear();
     }
-
+    if (name.compare("axe") == 0) {
+	return new SimpleAxe();
+    }
+    if (name.compare("crazy sword") == 0) {
+	return new RandomCrazySword();
+    }
+    if (name.compare("shillelagh") == 0) {
+	return new ShillelaghOfFreedom();
+    }
     throw "Invalid weapon";
 }
