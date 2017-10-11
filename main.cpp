5064a817893383bd5124af01813a7b16beaee2ca
will-nemo <will.lambert96@gmail.com>
2017-10-11 18:14:37 -0400

    Updated main file. Added the new weapons to it

--- main.cpp
+++ main.cpp
@@ -39,6 +39,18 @@ int main(int argc, char** argv) {
     simulateWeapon(weapon, armor);
     delete(weapon);
 
+    weapon = WeaponFactory::getInstance()->getWeapon("axe");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+
+    weapon = WeaponFactory::getInstance()->getWeapon("crazy sword");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+
+    weapon = WeaponFactory::getInstance()->getWeapon("shillelagh");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+
     return 0;
 }
 
